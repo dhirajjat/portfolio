97371be052021dc4aaf03f58a38555197e92d170
dhirajjat <dheerajjat9098@gmail.com>
2025-01-31 23:55:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+// Example JavaScript for interactivity
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+});
